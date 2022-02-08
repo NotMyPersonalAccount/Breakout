dc37cde9f2e32c5d336c0bfd58d585401ddf6e84
NotMyPersonalAccount <kevinzj710@gmail.com>
2022-02-07 21:03:40 -0800

    add restart button

--- breakout/views/PauseView.java
+++ breakout/views/PauseView.java
@@ -8,10 +8,16 @@ import static breakout.Sketch.*;
 
 public class PauseView extends View {
     public PauseView(Sketch app, View pausedView) {
-        super(app, new Input[]{new BasicButton(app, "Resume", BASE_TEXT_SIZE, BASE_TEXT_SIZE * 8, CANVAS_SIZE_X / 2f, CANVAS_SIZE_Y / 2f, () -> {
-            app.setView(pausedView);
-            app.loop();
-        })});
+        super(app, new Input[]{
+                new BasicButton(app, "Resume", BASE_TEXT_SIZE, BASE_TEXT_SIZE * 8, CANVAS_SIZE_X / 2f, CANVAS_SIZE_Y / 2f - BASE_TEXT_SIZE, () -> {
+                    app.setView(pausedView);
+                    app.loop();
+                }),
+                new BasicButton(app, "Restart", BASE_TEXT_SIZE, BASE_TEXT_SIZE * 8, CANVAS_SIZE_X / 2f, CANVAS_SIZE_Y / 2f + BASE_TEXT_SIZE, () -> {
+                    app.setView(new GameView(app));
+                    app.loop();
+                })
+        });
 
         this.draw();
         app.noLoop();
